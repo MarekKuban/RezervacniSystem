03e566981a4b20fd0883e245678fc343c1a40e5b
Mára Kubáň <kuban90287@oavm.cz>
2025-11-30 22:14:08 +0100

    implementace pesimistického zamykání

--- src/main/java/cz/rezervacnisystem/repository/JazykRepository.java
+++ src/main/java/cz/rezervacnisystem/repository/JazykRepository.java
@@ -1,7 +1,16 @@
 package cz.rezervacnisystem.repository;
 
 import cz.rezervacnisystem.model.Jazyk;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface JazykRepository extends JpaRepository<Jazyk, Integer> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT j FROM Jazyk j WHERE j.jazykId = :id")
+    Optional<Jazyk> findByIdWithLock(@Param("id") Integer id);
 }
